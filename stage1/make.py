0f909de2faf3ac154ba5697e29a1953fc4e2c35b
Nguyễn Hoàng Trung <trung@Trungs-MacBook-Pro.local>
2019-03-02 13:12:14 +0700

    Server and logging incl WebSocket working. Cleaned up.

---
+++ stage1/make.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+EXPORTS = [
+        {'path': 'index.html',  'content_type': 'text/html; charset=UTF-8', 'aliases': ['']},
+        {'path': 'pwn.html',    'content_type': 'text/html; charset=UTF-8'},
+        {'path': 'ready.js',    'content_type': 'text/javascript; charset=UTF-8'},
+        {'path': 'logging.js',  'content_type': 'text/javascript; charset=UTF-8'},
+        {'path': 'utils.js',    'content_type': 'text/javascript; charset=UTF-8'},
+        {'path': 'int64.js',    'content_type': 'text/javascript; charset=UTF-8'},
+        {'path': 'pwn.js',      'content_type': 'text/javascript; charset=UTF-8'},
+]
+
